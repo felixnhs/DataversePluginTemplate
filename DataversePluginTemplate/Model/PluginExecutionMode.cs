ef613a3839a4d7bb30e204e2f82f25785dc856d7
Zedane <42522808+Zedane@users.noreply.github.com>
2024-06-14 08:44:09 +0200

    Fix: ExecutionMode values

--- DataversePluginTemplate/Model/PluginExecutionMode.cs
+++ DataversePluginTemplate/Model/PluginExecutionMode.cs
@@ -10,14 +10,14 @@
         /// Dies bedeutet, dass die Ausführung des Plugins in Echtzeit erfolgt und 
         /// andere Operationen in der Pipeline anhalten, bis die Plugin-Ausführung abgeschlossen ist.
         /// </summary>
-        Synchronous = 1,
+        Synchronous = 0,
 
         /// <summary>
         /// Das Plugin wird asynchron ausgeführt. 
         /// Dies bedeutet, dass das Plugin in einer separaten Warteschlange ausgeführt wird und 
         /// die Hauptpipeline fortgesetzt wird, ohne auf die Ausführung des Plugins zu warten.
         /// </summary>
-        Asynchronous = 2,
+        Asynchronous = 1,
     }
 
 }
